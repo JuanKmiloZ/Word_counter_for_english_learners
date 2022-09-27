56c25f8ea63d347e799a6d8f843576edd01940d1
JUAN CAMILO ZULUAGA CASTAÑO <111556574+JuanKmiloZ@users.noreply.github.com>
2022-09-27 00:51:13 -0500

    Update Proyecto Contador de palabras-documentos en ingles.py

--- Proyecto Contador de palabras-documentos en ingles.py
+++ Proyecto Contador de palabras-documentos en ingles.py
@@ -2,19 +2,14 @@
 #Contador de palabras
 
 # pip install -U spacy
-# python -m spacy download en_core_web_sm
+# python -m spacy 
 import spacy
 
-# Load English tokenizer, tagger, parser and NER
+# Descargar y Cargar en_core_web_sm
 nlp = spacy.load("en_core_web_sm")
 
 # Process whole documents
-text = ("When Sebastian Thrun started working on self-driving cars at "
-        "Google in 2007, few people outside of the company took him "
-        "seriously. “I can tell you very senior CEOs of major American "
-        "car companies would shake my hand and turn away because I wasn’t "
-        "worth talking to,” said Thrun, in an interview with Recode earlier "
-        "this week.")
+text = input("Ingrese el texto: ")
 doc = nlp(text)
 
 # Los caracteres que no contamos como palabras
@@ -24,20 +19,20 @@ for caracter in quitar:
                           "")  # Remplazarlo por "nada"; es decir, removerlo
 
 
-# Lo convertimos a minúsculas pues una palabra mayúscula y minúscula cuenta como una sola
+# Convertimos toda palabra a minúscula
 text = text.lower()
 # Las palabras están separadas por un espacio así que convertimos la cadena a arreglo
 palabras = text.split(" ")
 
 # Ahora vamos a contar las palabras creando un diccionario. En este caso la clave del diccionario
 # será la palabra, y el valor será el conteo
-diccionario_frecuencias = {}
+Diccionario = {}
 for palabra in palabras:
-    if palabra in diccionario_frecuencias:
-        diccionario_frecuencias[palabra] += 1
+    if palabra in Diccionario:
+        Diccionario[palabra] += 1
     else:
-        diccionario_frecuencias[palabra] = 1
+        Diccionario[palabra] = 1
 
-for palabra in diccionario_frecuencias:
-    frecuencia = diccionario_frecuencias[palabra]
+for palabra in Diccionario:
+    frecuencia = Diccionario[palabra]
     print(frecuencia,palabra)
